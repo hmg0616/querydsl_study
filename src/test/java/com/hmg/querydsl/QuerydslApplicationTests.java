919935ce0d1f71edf80c58f7ddc1dbcff19f433a
mungyu <mungyu83@gmail.com>
2023-03-05 22:36:08 +0900

    Q 클래스 최초 생성 및 테스트 작성

---
+++ src/test/java/com/hmg/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,37 @@
+package com.hmg.querydsl;
+
+import com.hmg.querydsl.entity.Hello;
+import com.hmg.querydsl.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@SpringBootTest
+@Transactional
+class QuerydslApplicationTests {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Test
+    void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QHello qHello = QHello.hello; // static 생성자
+
+        Hello result = queryFactory
+                .selectFrom(qHello)
+                .fetchOne();
+
+        Assertions.assertThat(result).isEqualTo(hello);
+        Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
+    }
+
+}
