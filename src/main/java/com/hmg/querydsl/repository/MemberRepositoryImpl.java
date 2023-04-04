7bf962cba317e3a731eb9f22c1ebbed3f9f29c37
mungyu <mungyu83@gmail.com>
2023-04-05 01:56:55 +0900

    totalCount 쿼리 수정 (deprecated 함수 변경)

--- src/main/java/com/hmg/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/com/hmg/querydsl/repository/MemberRepositoryImpl.java
@@ -115,8 +115,8 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
                 .limit(pageable.getPageSize())
                 .fetch();
 
-        JPAQuery<Member> countQuery = queryFactory
-                .select(member)
+        JPAQuery<Long> countQuery = queryFactory
+                .select(member.count())
                 .from(member)
                 .leftJoin(member.team, team)
                 .where(
@@ -127,7 +127,7 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
                 );
 
 //        return new PageImpl<>(content, pageable, total);
-        return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchCount);
+        return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchOne);
     }
 
 }
